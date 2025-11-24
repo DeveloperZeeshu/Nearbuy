689cb6fd8d84af225c04d205d6ea900c9f83fd73
DeveloperZeeshu <itsabbaszeeshaan0604@gmail.com>
2025-11-24 23:52:15 +0530

    Fix Server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,22 @@
 import conf from './conf/conf.js'
 import app from './app.js'
 import { connectDB } from './config/db.js'
+import { Request, Response } from 'express'
 
-await connectDB()
+// await connectDB()
 
 // const PORT = conf.PORT || 3000
 // app.listen(PORT, () => console.log(`Server running at ${PORT} Port...`))
 
-export default app
+
+let isConnected = false
+
+export default async function handler(req: Request, res: Response) {
+    if (!isConnected) {
+        await connectDB()
+        isConnected = true
+    }
+
+    return app(req, res)
+}
+
