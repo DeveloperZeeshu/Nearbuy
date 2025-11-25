ad8980c8f4d052797f8e7b0b9ea58c5b4996aa01
DeveloperZeeshu <itsabbaszeeshaan0604@gmail.com>
2025-11-25 20:53:30 +0530

    fix protected route

--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -3,15 +3,19 @@ import { type ReactNode } from "react"
 import { useSelector } from "react-redux"
 
 import type { RootState } from "../store/store.js"
+import { Navigate } from "react-router-dom"
 
 interface ProtectedRouteProps {
     children: ReactNode
 }
 
 export const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
-    
+
     const accessToken = useSelector((state: RootState) => state.auth.accessToken)
 
-    return accessToken ? children : null
+    if (!accessToken)
+        <Navigate to='/' replace />
+
+    return children
 }
 
