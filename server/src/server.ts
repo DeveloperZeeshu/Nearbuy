b1d6122fa962add1579b95568d79a4c8fb0ef2e0
DeveloperZeeshu <itsabbaszeeshaan0604@gmail.com>
2025-12-18 19:05:27 +0530

    Vercel fix

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,12 +3,18 @@ import app from './app.js'
 import { connectDB } from './config/db.js'
 import { Request, Response } from 'express'
 
+export const config = {
+    runtime: 'nodejs'
+}
+
+//Local server setup
 // await connectDB()
 
 // const PORT = conf.PORT || 3000
 // app.listen(PORT, () => console.log(`Server running at ${PORT} Port...`))
 
 
+//Production server setup
 let isConnected = false
 
 export default async function handler(req: Request, res: Response) {
